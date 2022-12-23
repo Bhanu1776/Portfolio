dc74408cf3912306719c52541922d8c9dd06bf34
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-12-24 01:56:37 +0530

    :art: (App) Initial setup for portfolio
    Checking all stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import tw from "twin.macro";
 
-function App() {
+const Top = styled.div`
+  margin: 40px;
+  ${tw`text-red-600 bg-slate-200 `}
+`;
+
+const Button = tw.button`
+rounded-full
+p-4
+m-2
+bg-red-200
+cursor-crosshair
+text-green-800
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p id="s-specialty"> Our Team </p>
+      <h1 className="py-6 text-xl font-bold text-yellow-700 underline m-28">
+        Hello world!
+      </h1>
+      <Top>Bhanu Sunka</Top>
+      <Button> Press Me! </Button>
+      <div className="container text-red-600 bg-slate-500 ">Hello Bunny</div>
+    </>
   );
-}
+};
 
 export default App;
