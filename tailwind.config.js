62f6f195cb2c4ed302811567e010a99aae5fd103
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-12-24 13:04:05 +0530

    :wrench: (Tailwind) Added all the required config for the setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
+
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2.4rem",
+    },
+    debugScreens: {
+      position: ['bottom', 'left'],
+      selector: '.debug-screens',
+      style: {
+        backgroundColor: 'black',
+        color: 'white',
+      },
+    },
+    screens: {
+      xsm: '425px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      screens: {
+        'mobile': '425px',
+        'tablet': '640px',
+        'laptop': '1024px',
+        'desktop': '1280px',
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [require("tailwindcss-debug-screens"),
+  require("tailwindcss-brand-colors")],
+
 }
