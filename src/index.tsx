acf0deb3fcfa450193a5ac64ba5908cab0d5f497
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-12-24 13:05:17 +0530

    :art: (SC) Added Themeprovider with color values
    And integrated styled-components GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const theme = {
+  colors: {
+    bg: "#0000",
+    heading: "rgb(24 24 29)",
+    text: "rgb(24 24 29)",
+    black: " #212529",
+  },
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
