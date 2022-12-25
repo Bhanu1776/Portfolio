4eb2be57f44482feedb97f737a88f161e2b2484a
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-12-25 18:06:31 +0530

    :art: (Navbar) Almost done navbar
    Responsive navbar dark mode toggling left!

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,31 @@
 import Images from "../Images/Imgindex";
 import tw from "twin.macro";
+import styled from "styled-components";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const MiniDialog = styled.div`
+    display: ${() => (openMenu ? "block" : "none")};
+    ${tw`
+    fixed 
+    w-full 
+    max-w-sm 
+    p-9 
+    text-base 
+    font-semibold
+    text-gray-900 
+    bg-white 
+    shadow-lg 
+    rounded-3xl 
+    top-4 
+    right-4 
+    dark:bg-gray-800 
+    dark:text-gray-400
+  `}
+  `;
+
   return (
     <>
       <NavbarDiv>
@@ -36,12 +60,12 @@ const Navbar = () => {
                 Projects
               </span>
             </a>
-            <a href="/blog">
+            <a href="/skills">
               <span className="font-normal text-gray-500 dark:text-gray-400 'hidden md:inline-block rounded-full hover:text-gray-900 dark:hover:text-gray-200 transition-all">
                 Skills
               </span>
             </a>
-            <a href="/community-wall">
+            <a href="/contact">
               <span className="font-normal text-gray-500 dark:text-gray-400 'hidden md:inline-block rounded-full hover:text-gray-900 dark:hover:text-gray-200 transition-all">
                 Contact
               </span>
@@ -67,7 +91,7 @@ const Navbar = () => {
           </ButtonDiv>
 
           <div className="-my-2 -mr-2 md:hidden">
-            <Hamburger>
+            <Hamburger onClick={() => setOpenMenu(true)}>
               <span className="sr-only">Open menu</span>
               <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path
@@ -94,6 +118,69 @@ const Navbar = () => {
               </svg>
             </Hamburger>
           </div>
+
+          <MiniDialog>
+            <CloseBtn onClick={() => setOpenMenu(false)}>
+              <span className="sr-only">Close navigation</span>
+              <svg
+                viewBox="0 0 10 10"
+                className="w-4 h-4 overflow-visible"
+                aria-hidden="true"
+              >
+                <path
+                  d="M0 0L10 10M10 0L0 10"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                ></path>
+              </svg>
+            </CloseBtn>
+            <ul className="space-y-9">
+              <li>
+                <a href="/">
+                  <span className="text-2xl hover:text-teal-400 dark:hover:text-teal-500">
+                    Home
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="/about">
+                  <span className="text-2xl hover:text-teal-400 dark:hover:text-teal-500">
+                    About
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="/projects">
+                  <span className="text-2xl hover:text-teal-400 dark:hover:text-teal-500">
+                    Projects
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="/skills">
+                  <span className="text-2xl hover:text-teal-400 dark:hover:text-teal-500">
+                    Skills
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="/contact">
+                  <span className="text-2xl hover:text-teal-400 dark:hover:text-teal-500">
+                    Contact
+                  </span>
+                </a>
+              </li>
+            </ul>
+            <div className="pt-6 mt-6 border-t border-gray-200 dark:border-gray-200/10">
+              <DialogDarkBtn aria-label="Toggle Dark Mode" type="button">
+                <p className="pb-2 text-2xl font-semibold">
+                  Change to Dark Theme
+                </p>
+              </DialogDarkBtn>
+            </div>
+          </MiniDialog>
         </ActualNavbar>
       </NavbarDiv>
     </>
@@ -143,13 +230,22 @@ md:flex-1
 lg:w-0
 `;
 
-const DarkModeBtn = tw.div`
-p-5 
-border-2 
-border-coolGray-100 
-border-solid 
-rounded-full 
-bg-coolGray-200
+const DarkModeBtn = styled.div`
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 0.15s;
+
+  &:hover {
+    border: 2px solid black;
+  }
+  ${tw`
+    p-5 
+    border-2 
+    border-coolGray-100 
+    border-solid 
+    rounded-full 
+    bg-coolGray-200
+  `}
 `;
 
 const Hamburger = tw.div`
@@ -166,6 +262,32 @@ hover:text-gray-700
 hover:bg-gray-300 
 cursor-pointer 
 focus:outline-none 
+`;
+
+const CloseBtn = tw.button`
+absolute 
+flex 
+items-center 
+justify-center 
+w-8 
+h-8 
+text-gray-500 
+top-7 
+right-7 
+hover:text-gray-600 
+dark:text-gray-400 
+dark:hover:text-gray-300
+`;
+
+const DialogDarkBtn = tw.button`
+flex 
+items-center 
+justify-center 
+w-full 
+py-2 
+bg-gray-200 
+rounded-full 
+dark:bg-midnight-hover
 `;
 
 export default Navbar;
