26151c7a61cd3ae61254163cf1120cce21142dea
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-12-25 23:21:20 +0530

    :pencil: (Images) Updated with new images

--- src/Images/Imgindex.js
+++ src/Images/Imgindex.js
@@ -1,5 +1,11 @@
 const Images = {
     BLogo: require('./Bhanu-logo.png'),
+    HeroBg: require('./Hero-bg.png'),
+    FooterBg: require('./Footer-bg.png'),
+    PurpleBlur: require('./purpleblur.png'),
+    Ray1: require('./ray1.png'),
+    Ray2: require('./ray2.png'),
+    Ray3: require('./ray3.png'),
 }
 
 export default Images;
