e60ef5ca9dde72fc546bc43f94610384e747beec
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-01-04 18:03:05 +0530

    :recycle: (About) Refactored code, Now much simpler :)
    Heavily refacorted code now super simple, refactored all tailwind code to twin and fixed font for mobile
    , all images src moved to  LangDataa.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,229 +1,302 @@
 import styled from "styled-components";
+import tw from "twin.macro";
 import Images from "../Images/Imgindex";
+import { Languages, Web, Tools, Other } from "./LangData";
+// import { Tooltip } from "react-tooltip";
+// import "react-tooltip/dist/react-tooltip.css";
 
 const About = () => {
   return (
     <>
-      <div className="grid md:grid-cols-2 gap-2 sm:grid-row-2 ">
-        <div className="flex flex-col justify-center tablet:pr-12 xxsm:pr-10">
-          <h1 className="mobile:text-5xl xxsm:text-4xl font-headings tracking-wide font-bold leading-tight ">
-            A Bit About Me & My Skills
-          </h1>
-          <span className="bg-teal-500 mobile:w-[18rem] xxsm:w-[14rem] mt-4 min-w-20 h-2 bg-gradient-to-r from-teal-500 to-teal-300"></span>
+      <GridDiv>
+        <LeftGrid>
+          <LeftTitle>A Bit About Me & My Skills</LeftTitle>
+          <TitleDivider />
           <div className="mt-8">
-            <p className="font-sans tracking-wide">
+            <LeftPara>
               Hello! My name is Bhanu Sunka. I am a hard-working engineering
               student specializing in Information Technology from Xavier
               Institute of engineering, Mumbai University.
-            </p>
-            <p>
+            </LeftPara>
+            <LeftPara>
               I love to explore new technologies and leverage them to solve
               real-life problems. I'm a passionate learner who's always willing
               to learn and work across various technologies and domains. In my
               spare time, I love contributing to open-source projects.
-            </p>
-            <p>
+            </LeftPara>
+            <LeftPara>
               I am highly proficient in web development. I love to build
               responsive user-friendly websites that look visually appealing,
               with nice and fluid interactions, and close attention to User
               Experience. I constantly strive for the best outcome on any
               project I take on.
-            </p>
+            </LeftPara>
           </div>
-        </div>
+        </LeftGrid>
 
-        <div className="grid sm:grid-cols-2 gap-1 gap-x-14 sm:grid-rows-2 xxsm:mt-12 md:mt-0 xsm:grid-cols-1  ">
-          <img
-            src={Images.FooterBg}
-            alt=""
-            className="-scale-x-100 absolute right-0 mr-[-3.6rem] mt-[-13.7rem] tablet:h-[70rem] tablet:opacity-60 xxsm:h-[80rem] xxsm:opacity-40 -z-4"
-          />
-          <div className=" bg-[#f2e8f39e] drop-shadow-md hover:drop-shadow-none z-10 max-h-[40rem] xxsm:h-[19rem] tablet:h-[25rem] xxsm:rounded-3xl mb-3 tablet:mb-0 tablet:rounded-xl laptop:text-2xl hover:bg-transparent hover:border-2 hover:border-pink-300 hover:border-dashed transition ease-in-out duration-700">
-            <h1 className="flex font-mono justify-center mt-10 text-slate-700 font-bold text-[1.7rem]">
-              Programming Languages
-            </h1>
+        <RightGrid>
+          <BgImg src={Images.FooterBg} alt="" />
 
-            <div className="flex space-x-7 space-y-4 mt-8 flex-wrap mx-8">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                alt="javascript"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
-                alt="cplusplus"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
-                alt="java"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
-                alt="python"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
-                alt="typescript"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
-                alt="c"
-                width="40"
-              />
-            </div>
-          </div>
-          <div className="z-10 bg-[#fdffba59] drop-shadow-md hover:drop-shadow-none tablet:mt-14 max-h-[40rem] xxsm:h-[19rem] tablet:h-[25rem] xxsm:rounded-3xl mb-2 tablet:mb-0 tablet:rounded-xl text-2xl overflow-auto hover:bg-transparent hover:border-2 hover:border-yellow-300 hover:border-dashed transition ease-in-out duration-700">
-            <h1 className="flex font-mono text-slate-700 justify-center mt-10 font-bold text-[1.7rem]">
-              Web Development
-            </h1>
-            <div className="flex space-x-6 space-y-4 mt-8 flex-wrap mx-6 ">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"
-                alt="html5"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"
-                alt="css3"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg"
-                alt="sass"
-                width="40"
-              />
-              <img
-                src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
-                alt="tailwind-css"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg"
-                alt="bootstrap"
-                width="40"
-              />
-              <img
-                src="https://miro.medium.com/max/480/1*Iohnw2aOQ5EBghVoqKA7VA.png"
-                alt="Styled-Components"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-                alt="React"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg"
-                alt="NodeJs"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
-                alt="Express"
-                width="45"
-              />
-            </div>
-          </div>
-          <div className="z-10 bg-[#a8f9e84f] drop-shadow-md hover:drop-shadow-none mt-2 max-h-[40rem] xxsm:h-[19rem] tablet:h-[25rem] xxsm:rounded-3xl mb-2 tablet:mb-0 tablet:rounded-xl text-2xl hover:bg-transparent hover:border-2 hover:border-teal-300 hover:border-dashed transition ease-in-out duration-700">
-            <h1 className="flex font-mono text-slate-700 justify-center mt-10 font-bold text-[1.7rem]">
-              Tools
-            </h1>
-            <div className="flex space-x-7 space-y-4 mt-8 flex-wrap mx-8">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg"
-                alt="Git"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg"
-                alt="VsCode"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vim/vim-original.svg"
-                alt="Vim"
-                width="40"
-              />
-              <img
-                src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
-                alt="Vim"
-                width="40"
-              />
-              {/* <img
-                src="https://www.vectorlogo.zone/logos/jupyter/jupyter-ar21.svg"
-                alt="Jupyter"
-                width="60"
-                height="60"
-              /> */}
-            </div>
-          </div>
-          <div className="z-10 bg-[#d6cfff52] drop-shadow-md hover:drop-shadow-none tablet:mt-14 max-h-[40rem] xxsm:h-[19rem] tablet:h-[25rem] xxsm:rounded-3xl mb-2 tablet:mb-0 tablet:rounded-xl text-2xl overflow-auto hover:bg-transparent hover:border-2 hover:border-purple-400 hover:border-dashed transition ease-in-out duration-700">
-            <h1 className="flex font-mono text-slate-700 justify-center mt-10 font-bold text-[1.7rem]">
-              Other
-            </h1>
-            <div className="flex space-x-6 space-y-4 mt-8 flex-wrap mx-7">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
-                alt="mongodb"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
-                alt="mysql"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
-                alt="aws"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jenkins/jenkins-original.svg"
-                alt="jenkins"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bash/bash-original.svg"
-                alt="bash"
-                width="40"
-              />
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
-                alt="linux"
-                width="40"
-              />
-              <img
-                src="https://cdn.worldvectorlogo.com/logos/pug.svg"
-                alt="pugjs"
-                width="40"
-              />
-              <img src="https://mui.com/static/logo.png" alt="MUI" width="40" />
-            </div>
-          </div>
-        </div>
-      </div>
+          <Card1>
+            <CardHeading>Programming Languages</CardHeading>
+            <LangDiv1>
+              {Languages.map((val) => {
+                return <img src={val.src} alt={val.alt} width={val.width} />;
+              })}
+            </LangDiv1>
+          </Card1>
+
+          <Card2>
+            <CardHeading>Web Development</CardHeading>
+            <LangDiv2>
+              {Web.map((val) => {
+                return <img src={val.src} alt={val.alt} width={val.width} />;
+              })}
+            </LangDiv2>
+          </Card2>
+
+          <Card3>
+            <CardHeading>Tools</CardHeading>
+            <LangDiv3>
+              {Tools.map((val) => {
+                return <img src={val.src} alt={val.alt} width={val.width} />;
+              })}
+            </LangDiv3>
+          </Card3>
+
+          <Card4>
+            <CardHeading>Other</CardHeading>
+            <LangDiv4>
+              {Other.map((val) => {
+                return <img src={val.src} alt={val.alt} width={val.width} />;
+              })}
+            </LangDiv4>
+          </Card4>
+        </RightGrid>
+      </GridDiv>
       <Hr />
     </>
   );
 };
 
+const GridDiv = tw.div`
+grid 
+gap-2 
+md:grid-cols-2 
+// sm:grid-rows-2 
+`;
+
+const LeftGrid = tw.div`
+flex 
+flex-col 
+justify-center 
+xxsm:pr-10
+tablet:pr-14 
+`;
+
+const LeftTitle = tw.h1`
+font-headings 
+tracking-wide 
+font-bold 
+leading-tight 
+xxsm:text-4xl 
+mobile:text-5xl 
+`;
+
+const TitleDivider = tw.span`
+bg-teal-500 
+mt-4 
+h-2 
+bg-gradient-to-r 
+from-teal-500 
+to-teal-300
+// min-w-20 
+mobile:w-[18rem] 
+xxsm:w-[14rem] 
+`;
+
+const LeftPara = tw.p`
+font-sans 
+tracking-wide 
+text-justify
+`;
+
+const RightGrid = tw.div`
+grid 
+gap-1 
+gap-x-14 
+xxsm:mt-12 
+xsm:grid-cols-1 
+sm:grid-cols-2 
+sm:grid-rows-2 
+md:mt-0 
+`;
+
+const BgImg = tw.img`
+absolute 
+right-0 
+-scale-x-100 
+mr-[-3.6rem] 
+mt-[-13.7rem] 
+xxsm:h-[80rem] 
+xxsm:opacity-40 
+tablet:h-[70rem] 
+tablet:opacity-60 
+// -z-40
+`;
+
+const Card1 = tw.div`
+bg-[#f2e8f39e] 
+z-10 
+mb-3 
+max-h-[40rem] 
+drop-shadow-xl
+hover:drop-shadow-none 
+hover:bg-transparent 
+hover:border-2 
+hover:border-pink-300 
+hover:border-dashed 
+transition 
+ease-in-out 
+duration-700
+xxsm:h-[19rem] 
+xxsm:rounded-3xl 
+tablet:h-[25rem] 
+tablet:mb-0 
+tablet:rounded-xl 
+laptop:text-2xl 
+`;
+
+const CardHeading = tw.h1`
+flex 
+font-mono 
+justify-center 
+mt-10 
+text-slate-700 
+font-bold 
+text-[1.7rem]
+`;
+
+const LangDiv1 = tw.div`
+flex 
+flex-wrap 
+space-x-7 
+space-y-4 
+mt-8 
+mx-8
+`;
+
+const Card2 = tw.div`
+bg-[#fdffba59] 
+z-10 
+overflow-auto 
+drop-shadow-2xl
+text-2xl 
+max-h-[40rem] 
+mb-2 
+hover:drop-shadow-none 
+hover:bg-transparent 
+hover:border-2 
+hover:border-yellow-300 
+hover:border-dashed 
+transition 
+ease-in-out 
+duration-700
+xxsm:rounded-3xl 
+xxsm:h-[19rem] 
+tablet:mt-14 
+tablet:h-[25rem] 
+tablet:rounded-xl 
+tablet:mb-0 
+`;
+
+const LangDiv2 = tw.div`
+flex 
+space-x-6 
+space-y-4 
+mt-8 
+flex-wrap 
+mx-6
+`;
+
+const Card3 = tw.div`
+bg-[#a8f9e84f] 
+drop-shadow-2xl
+z-10 
+mt-2 
+max-h-[40rem] 
+mb-2 
+text-2xl 
+hover:bg-transparent 
+hover:border-2 
+hover:border-teal-300 
+hover:border-dashed 
+hover:drop-shadow-none 
+transition 
+ease-in-out 
+duration-700
+xxsm:rounded-3xl 
+xxsm:h-[19rem] 
+tablet:mb-0 
+tablet:rounded-xl 
+tablet:h-[25rem] 
+`;
+
+const LangDiv3 = tw.div`
+flex 
+flex-wrap 
+space-x-7 
+space-y-4 
+mx-8
+mt-8 
+`;
+
+const Card4 = tw.div`
+bg-[#d6cfff52] 
+z-10 
+overflow-auto 
+drop-shadow-xl
+text-2xl 
+max-h-[40rem] 
+mb-2 
+hover:drop-shadow-none 
+hover:bg-transparent 
+hover:border-2 
+hover:border-purple-400 
+hover:border-dashed 
+transition 
+ease-in-out 
+duration-700
+xxsm:h-[19rem] 
+xxsm:rounded-3xl 
+tablet:mt-14 
+tablet:h-[25rem]
+tablet:mb-0 
+tablet:rounded-xl 
+`;
+
+const LangDiv4 = tw.div`
+flex 
+flex-wrap 
+space-x-6 
+space-y-4 
+mx-7
+mt-8 
+`;
+
 const Hr = styled.hr`
+  border-style: none;
+  text-align: center;
   margin-top: 4rem;
   margin-bottom: 6.4rem;
   height: 3.5rem;
   width: 100%;
-  border-style: none;
-  text-align: center;
 
   &::before {
+    content: "∿∿∿";
+    color: rgb(209 213 219 / 1);
     font-size: 2.4rem;
     line-height: 3.2rem;
-    color: rgb(209 213 219 / 1);
-    content: "∿∿∿";
   }
 `;
 
