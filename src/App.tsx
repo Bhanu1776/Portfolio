1b58d82e76ebeaa735de5dd168181b1b1193b707
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-01-15 20:25:05 +0530

    :recycle: (App) Refactored Imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 import tw from "twin.macro";
-import About from "./components/About";
-import Blog from "./components/Blog";
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import { Project } from "./components/Project";
 import Work from "./components/Work";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -25,7 +25,6 @@ const App = () => {
 const MainDiv = styled.main`
   max-width: 115rem;
   ${tw`relative flex flex-col min-h-screen px-6 mx-auto pt-[8rem] bg-white `};
-  /* ${tw`border-2 border-black`}; */
 `;
 
 export default App;
