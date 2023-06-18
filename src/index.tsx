64a14b8bd7f5d3061f3e8774d78d877ed01c64c3
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-06-18 12:22:29 +0530

    build(Google Analytics): migrated to latest GA4

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,11 @@ import { ThemeProvider } from "styled-components";
 import Navbar from "./components/Navbar";
 import reportWebVitals from "./reportWebVitals";
 import {Analytics} from '@vercel/analytics/react'
-import ReactGa from "react-ga"
+import ReactGa from "react-ga4"
 
-const TRACKING_ID = "UA-257193459-1";
+const TRACKING_ID = 'G-RQ3FJ2BLCG';
 ReactGa.initialize(TRACKING_ID);
+ReactGa.send({ hitType: "pageview", page: "/", title: "Home Page" });
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
