f5198c61ed821142b8cb6917fda19ca4ed234337
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-08-09 14:26:07 +0530

    refactor: cleaned code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import tw from "twin.macro";
-import Images from "../Images/Imgindex";
 import Hr from "./Hr";
 import { Link } from "react-scroll";
 import ReactGa from "react-ga4"
