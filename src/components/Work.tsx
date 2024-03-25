e1a3a1924c58cd9e2e4d6462deb88eeb5e9c725f
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-03-25 22:37:33 +0530

    chore: Updated Work Experience

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,7 +1,7 @@
-import tw from "twin.macro";
-import { Button } from "./Project";
-import Hr from "./Hr";
 import ReactGa from "react-ga4";
+import tw from "twin.macro";
+import Hr from "./Hr";
+import { Button } from "./Project";
 
 const Work = () => {
   return (
@@ -36,43 +36,121 @@ const Work = () => {
                 ></path>
               </svg>
               <a
+                href="https://drive.google.com/file/d/13vIrjnTaNCF3GDbOpnyO7i07u0kJtUKV/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => {
+                  ReactGa.event({
+                    category: "Parassolutions Internship",
+                    action: "Checking Internship repository - parassolutions",
+                  });
+                }}
+              >
+                <h3 className="p-0 m-0 pt-2 text-4xl font-bold mb-4 dark:text-gray-200">
+                  React Developer Intern
+                </h3>
+              </a>
+              <a
+                href="https://parassolutions.in/"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => {
+                  ReactGa.event({
+                    category: "Parassolutions Website",
+                    action: "parassolutions website",
+                  });
+                }}
+              >
+                <p className="p-0 m-0 text-3xl mb-2 dark:text-gray-200 hover:dark:text-teal-500">
+                  Paras Solutions
+                </p>
+              </a>
+              <p className="p-0 m-0 text-2xl dark:text-gray-200 text-gray-500 before:content-['['] dark:before:text-teal-500 before:text-indigo-500 after:text-indigo-500 after:font-bold before:font-bold after:content-[']'] dark:after:text-teal-500 after:text-indigo-500after:font-bold">
+                &nbsp; November 2023 &nbsp;
+              </p>
+            </div>
+          </div>
+          <p className="relative pb-10 md:border-b dark:text-gray-200 md:border-gray-300 md:dark:border-gray-400 md:col-span-3 text-[1.7rem] font-sans">
+            - Worked on two major client projects during the internship period.
+            <br />
+            {/* <span className="font-semibold text-slate-800 dark:text-gray-200">
+              Achievements: &nbsp;
+            </span> */}
+            - Initiated the development process from scratch using React.js and Typescript, Integrated CRUD operations using Redux-toolkit to enable seamless data management. <br />
+            {/* <span className="font-semibold text-slate-800 dark:text-gray-200">
+              Contact: &nbsp;
+            </span> */}
+            - Delivered a user-friendly and efficient portal, reducing manual processes and enhancing overall productivity for financial institutions.
+          </p>
+        </Article>
+
+        {/* <div className="mt-16"> */}
+        <Article>
+          <div className="relative pb-12 md:col-span-2">
+            <div className="sticky top-28 mt-20">
+              <svg
+                className="absolute left-[-38px] md:left-[-50px] color-bg-default color-fg-muted bg-white dark:bg-dark rounded-full dark:text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="28"
+                height="28"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15.5 11.75a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0zm1.444-.75a5.001 5.001 0 00-9.888 0H2.75a.75.75 0 100 1.5h4.306a5.001 5.001 0 009.888 0h4.306a.75.75 0 100-1.5h-4.306z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <a
                 href="https://github.com/Bhanu1776/IT-Dept-Website"
                 target="_blank"
                 rel="noreferrer"
                 onClick={() => {
                   ReactGa.event({
-                    category: "Internships",
+                    category: "CLG WEBSITE REPO",
                     action: "Checking Internship repository",
                   });
                 }}
               >
                 <h3 className="p-0 m-0 pt-2 text-4xl font-bold mb-4 dark:text-gray-200">
-                  Full Stack Developer
+                  SDE Intern
                 </h3>
               </a>
-              <p className="p-0 m-0 text-3xl mb-2 dark:text-gray-200">
-                Xavier Institute of Engineering
-              </p>
+              <a
+                href="https://itjobxs.com/"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => {
+                  ReactGa.event({
+                    category: "CLG WEBSITE REPO",
+                    action: "Checking Internship repository",
+                  });
+                }}
+              >
+                <p className="p-0 m-0 text-3xl mb-2 dark:text-gray-200 hover:dark:text-teal-500">
+                  ITJOBXS
+                </p>
+              </a>
               <p className="p-0 m-0 text-2xl dark:text-gray-200 text-gray-500 before:content-['['] dark:before:text-teal-500 before:text-indigo-500 after:text-indigo-500 after:font-bold before:font-bold after:content-[']'] dark:after:text-teal-500 after:text-indigo-500after:font-bold">
                 &nbsp; April 2023 &nbsp;
               </p>
             </div>
           </div>
-          <p className="relative pb-4 md:border-b dark:text-gray-200 md:border-gray-300 md:dark:border-gray-400 md:col-span-3 text-[1.7rem] font-sans">
-            Developed a full-fledged college web app for the IT Department.
+          <p className="relative mt-20 pb-10 md:border-b dark:text-gray-200 md:border-gray-300 md:dark:border-gray-400 md:col-span-3 text-[1.7rem] font-sans">
+            - Worked on the design and development part of the college management web application.
             <br />
             {/* <span className="font-semibold text-slate-800 dark:text-gray-200">
               Achievements: &nbsp;
             </span> */}
-            Build with Nextjs13, Firebase for backend, and Ant-design as UI
-            component Library. <br />
+            - Achieved a powerful combination of Next.js, Firebase, and Ant Design, resulting in a dynamic, scalable, and visually appealing web application. <br />
             {/* <span className="font-semibold text-slate-800 dark:text-gray-200">
               Contact: &nbsp;
             </span> */}
-            Led the technical part of the project from design, and management to
-            deployment.
+            - Enabled administrators to create, update, and manage course offerings, schedules, and syllabus within the system.
           </p>
         </Article>
+        {/* </div> */}
+
         <Button
           className="group cursor-pointer"
           href="https://github.com/Bhanu1776"
