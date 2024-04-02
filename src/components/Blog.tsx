d4184b7c4d73396066eb81e7477b40ea185932eb
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-04-03 01:46:12 +0530

    chore(Blog): Added all articles from medium and dev.com

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -5,8 +5,40 @@ const Blog = () => {
     <>
       <Div id="Blog">
         <H2>Blogs</H2>
-        <H1>Coming Soon</H1>
-        <P>Stay Tuned!</P>
+        <div className="text-white mt-16 gap-10 text-3xl grid grid-cols-2">
+          <a href="https://dev.to/bhanu1776/tailwind-in-styled-components-29j" target="_blank" rel="noreferrer">
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
+              <h1 className="text-slate-200 font-semibold">Tailwindcss in Styled-Components</h1>
+              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#twin #styled-components #tailwindcss #css</p>
+            </div>
+          </a>
+          <a href="https://dev.to/bhanu1776/the-best-vscode-snippets-for-react-typescript-nextjs-redux-toolkit-5gmm" target="_blank" rel="noreferrer">
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
+              <h1 className="text-slate-200">The Best Vscode Snippets for React Typescript + Nextjs + Redux Toolkit</h1>
+              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#vscode #reactjs #nextjs #redux</p>
+            </div>
+          </a>
+          <a href="https://medium.com/p/b090485026d4" target="_blank" rel="noreferrer">
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
+              <h1 className="text-slate-200">10 UNIQUE Mac Apps You Can't Live Without!</h1>
+              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #unique #productivity</p>
+            </div>
+          </a>
+          <a href="https://medium.com/p/0468d3ca5b9b" target="_blank" rel="noreferrer">
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
+              <h1 className="text-slate-200">10 Mac Apps I Regret Not Using Sooner</h1>
+              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #best #productivity</p>
+            </div>
+          </a>
+          <a href="https://medium.com/p/f33171825f40" target="_blank" rel="noreferrer">
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
+              <h1 className="text-slate-200">Top Free Utility Mac Apps You Aren't Using</h1>
+              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #productivity #free</p>
+            </div>
+          </a>
+          
+        </div>
+        {/* <P>Stay Tuned!</P> */}
       </Div>
     </>
   );
@@ -15,8 +47,8 @@ const Blog = () => {
 const Div = tw.main`
   bg-[#F8FAFC] 
   border-slate-200 
-  px-6 
-  py-6
+  !px-20
+  !py-16
   rounded-[1.9rem]
   mb-24
   drop-shadow-md
