2f9e08917dbdc7da8aad479ab145e561dfb2ff8e
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-04-03 01:55:27 +0530

    fix: Light mode for blog section

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -5,35 +5,35 @@ const Blog = () => {
     <>
       <Div id="Blog">
         <H2>Blogs</H2>
-        <div className="text-white mt-16 gap-10 text-3xl grid grid-cols-2">
+        <div className="dark:text-white mt-16 gap-10 text-3xl grid grid-cols-2">
           <a href="https://dev.to/bhanu1776/tailwind-in-styled-components-29j" target="_blank" rel="noreferrer">
-            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
-              <h1 className="text-slate-200 font-semibold">Tailwindcss in Styled-Components</h1>
-              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#twin #styled-components #tailwindcss #css</p>
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-purple-800 dark:hover:border-teal-500">
+              <h1 className="dark:text-slate-200 font-semibold">Tailwindcss in Styled-Components</h1>
+              <p className="dark:text-neutral-300 tracking-wider text-[1.4rem]">#twin #styled-components #tailwindcss #css</p>
             </div>
           </a>
           <a href="https://dev.to/bhanu1776/the-best-vscode-snippets-for-react-typescript-nextjs-redux-toolkit-5gmm" target="_blank" rel="noreferrer">
-            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
-              <h1 className="text-slate-200">The Best Vscode Snippets for React Typescript + Nextjs + Redux Toolkit</h1>
-              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#vscode #reactjs #nextjs #redux</p>
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-purple-800 dark:hover:border-teal-500">
+              <h1 className="dark:text-slate-200">The Best Vscode Snippets for React Typescript + Nextjs + Redux Toolkit</h1>
+              <p className="dark:text-neutral-300 tracking-wider text-[1.4rem]">#vscode #reactjs #nextjs #redux</p>
             </div>
           </a>
           <a href="https://medium.com/p/b090485026d4" target="_blank" rel="noreferrer">
-            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
-              <h1 className="text-slate-200">10 UNIQUE Mac Apps You Can't Live Without!</h1>
-              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #unique #productivity</p>
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-purple-800 dark:hover:border-teal-500">
+              <h1 className="dark:text-slate-200">10 UNIQUE Mac Apps You Can't Live Without!</h1>
+              <p className="dark:text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #unique #productivity</p>
             </div>
           </a>
           <a href="https://medium.com/p/0468d3ca5b9b" target="_blank" rel="noreferrer">
-            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
-              <h1 className="text-slate-200">10 Mac Apps I Regret Not Using Sooner</h1>
-              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #best #productivity</p>
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-purple-800 dark:hover:border-teal-500">
+              <h1 className="dark:text-slate-200">10 Mac Apps I Regret Not Using Sooner</h1>
+              <p className="dark:text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #best #productivity</p>
             </div>
           </a>
           <a href="https://medium.com/p/f33171825f40" target="_blank" rel="noreferrer">
-            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-teal-500">
-              <h1 className="text-slate-200">Top Free Utility Mac Apps You Aren't Using</h1>
-              <p className="text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #productivity #free</p>
+            <div className="border-2 max-w-3xl p-10 border-slate-500 text-left ease-in-out transition-all duration-200 rounded-2xl justify-between w-full hover:border-purple-800 dark:hover:border-teal-500">
+              <h1 className="dark:text-slate-200">Top Free Utility Mac Apps You Aren't Using</h1>
+              <p className="dark:text-neutral-300 tracking-wider text-[1.4rem]">#macos #macapps #productivity #free</p>
             </div>
           </a>
           
@@ -69,31 +69,6 @@ const H2 = tw.h2`
   mt-6
   tablet:text-7xl 
   dark:text-teal-500
-`;
-
-const H1 = tw.h1`
-  text-slate-600 
-  text-6xl
-  font-headings 
-  tracking-wide 
-  font-extrabold 
-  opacity-50 
-  tablet:text-[7.5rem] 
-  tablet:mt-10
-  dark:text-slate-400 
-`;
-
-const P = tw.p`
-  text-violet-600
-  font-sans
-  font-bold
-  px-1
-  pt-2
-  mb-2
-  tablet:text-3xl
-  tablet:px-3
-  tablet:mt-8
-  dark:text-violet-500
 `;
 
 export default Blog;
