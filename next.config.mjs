7eca4206f0971f77e178fd90690f05f4681542c9
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-07-27 12:58:42 +0530

    build: completely setup build with nextjs
    - Add Nextjs 14.2.5
    - Add Eslint, prettier, prettier-tailwind
    - Pwa support

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  ...pwaConfig,
+  eslint: {
+    dirs: ['src'],
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const temp = 1;
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Bhanu portfolio</h1>
+    </div>
+  );
+};
+
+export default page;
